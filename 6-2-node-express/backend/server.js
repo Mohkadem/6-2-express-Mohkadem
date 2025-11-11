08eb219d589fc09937108290eedb90100983144b
Mohkadem <s202245820@kfupm.edu.sa>
2025-11-11 16:47:27 +0300

    server is done

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
+const app = express();
+app.use(cors());
 
-{/*write code for cors*/}
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
+{
+  app.get("/", (req, res) => {
+    res.send("Welcome to the Quote Generator API");
+  });
+}
 
-{/*write code to define routes*/}
-
-
-{/*write code to create server*/}
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
